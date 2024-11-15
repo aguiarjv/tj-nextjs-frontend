735a6aacbb0e37fd9fb93567557a264a02cbcb23
aguiarjv <jvictoraguiar@live.com>
2024-11-15 18:03:06 -0300

    changed default theme to indigo

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,7 +7,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex min-h-screen bg-card dark:bg-slate-800/20">
+    <div className="flex min-h-screen bg-card">
       <SideMenu />
       <ScrollArea className="max-h-screen w-full py-8 px-32 3xl:px-72">
         {children}
