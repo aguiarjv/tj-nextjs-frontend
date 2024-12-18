396aef2c76c7ba7666971c693d8008969c7eaa2b
aguiarjv <jvictoraguiar@live.com>
2024-12-18 20:53:18 -0300

    small changes

--- frontend/src/app/[locale]/dashboard/strategies/page.tsx
+++ frontend/src/app/[locale]/dashboard/strategies/page.tsx
@@ -1,3 +1,17 @@
+import { NewStrategyDialog } from "@/components/strategies/new-strategy";
+import { Separator } from "@/components/ui/separator";
+
 export default function Strategies() {
-  return <p>strategies</p>;
+  return (
+    <div>
+      <div className="flex flex-row items-center justify-between mb-2">
+        <h1 className="text-2xl font-bold text-secondary-foreground">
+          Estratégias
+        </h1>
+        <NewStrategyDialog />
+      </div>
+      <Separator className="bg-black/25 dark:bg-white/25 mb-5" />
+      <div>lista de estrategias</div>
+    </div>
+  );
 }
